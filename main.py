8f6df3cce844db6566e38775a95aedcdf34adf26
joshadams07 <joshua.adams@warriorlife.net>
2020-08-05 18:53:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import turtle
+name = turtle.Turtle()
+name.speed(10)
+name.color(0, 5, 200)
+for i in range(4):
+  name.forward(100)
+  name.right(90)
+name.penup()
+name.goto(50,50)
+name.pendown()
+name.color(200,20, 20)
+for i in range(3):
+  name.forward(100)
+  name.right(120)
+
+  import random
+  for i in range(5):
+    num = random.randint(1, 10)
+    print(num)
+
+
